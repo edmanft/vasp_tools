df197155ac81aa31c37d3569208faffea888bd41
MGonzalez <68707756+edmanft@users.noreply.github.com>
2023-05-11 12:45:30 +0200

    Update visualization.py

--- utils/visualization.py
+++ utils/visualization.py
@@ -46,3 +46,19 @@ def parse_formula(formula):
             counts[symbol] = count
 
     return counts
+
+def plot_from_outcar(path, ylim_E=None, ylim_F=None):
+
+    outcar_full = read(path, format='vasp-out', index=':')
+
+    energies = [step.get_total_energy() for step in outcar_full]
+    forces = [(step.get_forces()**2).sum() for step in outcar_full]
+
+    fig, ax = plt.subplots(2,1)
+    ax[0].plot(np.arange(len(energies))+1, energies, 'g-',label='Energy $(eV)$')
+    ax[1].plot(np.arange(len(forces))+1, forces, 'b-', label='Forces $(eV/\AA³)$')
+    if ylim_E is not None:
+        ax[0].set_ylim(ylim_E)
+    if ylim_F is not None:
+        ax[1].set_ylim(ylim_F)
+    fig.legend()
